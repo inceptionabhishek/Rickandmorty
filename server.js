6f6ad7b5ccac5578e4177f5b3fcf7b6a2fcaadee
Abhishek kumar <abhishek1122338@gmail.com>
2022-03-23 10:03:04 +0530

    Ready for production

--- server.js
+++ server.js
@@ -18,5 +18,12 @@ connection.once("open", () => {
 
 app.use("/favourite", require("./routes/favourite.js"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
